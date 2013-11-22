acae21de9748c7e97b694657f36b53c0ede27efd
mmontone <marianomontone@gmail.com>
2013-11-22 15:34:28 -0300

    Housekeeping

--- src/xul.lisp
+++ src/xul.lisp
@@ -129,4 +129,95 @@
    crop disabled href
    (value :initarg :value
 	  :accessor value)))
+
+(defmethod serialize-xul ((window window))
+  (cxml:with-element "window"
+    (cxml:attribute "xmlns" "http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul")
+    (loop for child in (children window)
+	 do (serialize-xul child))))
+
+(defmethod serialize-xul ((label label))
+  (cxml:with-element "label"
+    (cxml:attribute "control" (control label))
+    (cxml:attribute "accesskey" (accesskey label))
+    (cxml:attribute "value" (value label))))
+
+(defmethod serialize-xul ((vbox vbox))
+  (cxml:with-element "vbox"
+    (loop for child in (children vbox)
+	 do (serialize-xul child))))
 	    
+(defun make-app-folder (app path)
+  (ensure-directories-exist path)
+  (let* ((chrome-path (merge-pathnames #p"chrome/" path))
+	 (defaults-path (merge-pathnames #p"defaults/" path))
+	 (content-path (merge-pathnames #p"content/" chrome-path))
+	 (preferences-path (merge-pathnames #p"preferences/" defaults-path)))
+    (ensure-directories-exist chrome-path)
+    (ensure-directories-exist content-path)
+    (ensure-directories-exist defaults-path)
+    (ensure-directories-exist preferences-path)
+
+    (with-open-file (stream (merge-pathnames "application.ini" path) :direction :output
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (generate-initialization app stream))
+
+    (with-open-file (stream (merge-pathnames "chrome.manifest" path) :direction :output
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (generate-chrome-manifest app stream))
+
+    (with-open-file (stream (merge-pathnames "prefs.js" preferences-path) :direction :output
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (generate-preferences app stream))
+
+    (with-open-file (stream (merge-pathnames "main.xul" content-path) :direction :output
+			    :if-exists :supersede
+			    :if-does-not-exist :create)
+      (generate-xul app stream))))
+
+(defun generate-initialization (app stream)
+  (format stream "[App]~%")
+  (format stream "Vendor=~a~%" (vendor app))
+  (format stream "Name=~a~%" (name app))
+  (format stream "Version=~a~%" (version app))
+  (format stream "BuildID=~a~%" (build-id app))
+  (format stream "ID=~a~%" (id app))
+  (format stream "~%[Gecko]~%")
+  (format stream "MinVersion=~a~%" (gecko-min-version app))
+  (format stream "MaxVersion=~a~%" (gecko-max-version app)))
+
+(defun generate-chrome-manifest (app stream)
+  (format stream "content myapp file:chrome/content/"))
+
+(defun generate-preferences (app stream)
+  (format stream "pref(\"toolkit.defaultChromeURI\", \"chrome://myapp/content/main.xul\");~%")
+  (format stream "/* debugging prefs, disable these before you deploy your application! */~%")
+  (format stream "pref(\"browser.dom.window.dump.enabled\", true);~%")
+  (format stream "pref(\"javascript.options.showInConsole\", true);~%")
+  (format stream "pref(\"javascript.options.strict\", true);~%")
+  (format stream "pref(\"nglayout.debug.disable_xul_cache\", true);~%")
+  (format stream "pref(\"nglayout.debug.disable_xul_fastload\", true);~%"))
+
+(defun generate-xul (app stream)
+  (let ((output (make-xul-character-stream-sink stream)))
+      (cxml:with-xml-output output
+	(serialize-xul (top app)))))
+
+(defun run-app (app)
+  (let ((app-folder (pathname (format nil "/tmp/~A/" (name app)))))
+    (make-app-folder app app-folder)
+    (sb-ext:run-program "/usr/bin/xulrunner" (list "-app" (format nil"~Aapplication.ini" app-folder)))))
+
+(defmacro xul (xul)
+  `(make-xul ',xul))
+
+(defun make-xul (xul)
+  (destructuring-bind (xul-element attributes &rest children) xul
+    (if children
+	(apply #'make-instance xul-element (cons :children
+						 (cons (mapcar #'make-xul children)
+						       attributes)))
+	(apply #'make-instance xul-element attributes))))
