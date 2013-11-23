c0a497844e0525952042db5698b07da65815fa3f
mmontone <marianomontone@gmail.com>
2013-11-23 14:31:42 -0300

    Generic xul element serialization

--- src/controls/control.lisp
+++ src/controls/control.lisp
@@ -13,12 +13,14 @@
 	 (class* :attribute-name "class") coalesce-duplicate-arcs collapsed container containment context
 	 context-menu data-sources dir empty equal-size flags flex height hidden id
 	 insert-after insert-before left max-height max-width menu min-height min-width mouse-through
-	 observes ordinal (orient :initarg :orient
-	   :initform nil
-	   :accessor orient
-	   :type (or :horizontal :vertical)) pack persist popup (position* :attribute-name "position") preference-editable query-type
+	 observes ordinal
+	 (orient :type (or :horizontal :vertical) :documentation "Orientation") pack persist popup (position* :attribute-name "position") preference-editable query-type
 	 ref remove-element sort-direction sort-resource sort-resource-2 status-text style
-	 template tooltip tooltip-text top uri wait-cursor width ))
+	 template tooltip tooltip-text top uri wait-cursor width
+
+	 ;; Events
+	 (on-command :attribute-name "oncommand")
+	 ))
 
 (define-xul-element container-element (xul-element)
   ((children :initarg :children
@@ -28,3 +30,25 @@
 
 (define-xul-element xul-control (xul-element)
   ())
+
+(defgeneric serialize-xul (xul-element))
+
+(defmethod serialize-xul ((xul-element xul-element))
+  ;; Generic serialization
+  (let ((element-name (string-downcase (remove #\- (symbol-name (class-name (class-of xul-element)))))))
+    (cxml:with-element element-name
+      (loop for attribute in (attributes (class-of xul-element))
+	 when (slot-boundp xul-element (closer-mop:slot-definition-name attribute))
+	 do
+	   (cxml:attribute (attribute-name attribute)
+			   (serialize-xml-value
+			    (slot-value xul-element (closer-mop:slot-definition-name attribute))))))))
+
+(defmethod serialize-xml-value ((value (eql nil)))
+  "false")
+
+(defmethod serialize-xml-value ((value (eql t)))
+  "true")
+
+(defmethod serialize-xml-value (x)
+  (princ-to-string x))
