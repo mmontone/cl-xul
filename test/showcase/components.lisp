5bf638c2e9b0c34faa7269f6c3392162c07a3de5
mmontone <marianomontone@gmail.com>
2013-11-30 09:09:52 -0300

    Multi counters as components showcase

--- test/showcase/components.lisp
+++ test/showcase/components.lisp
@@ -2,15 +2,37 @@
 
 (define-component components-showcase ()
   ()
+  (:initialize (comp)
+	       (add-component comp (gensym) (make-instance 'counter :counter 0)))
   (:render (comp)
-	   (<:hbox
-	     (loop for child being the hash-values of (children comp) do
-		  (render comp))
+	   (<:vbox
+	     (<:vbox
+	       (maphash (lambda (slot counter)
+			  (declare (ignore slot))
+			  (render counter)
+			  (<:button (<:label= "Remove")
+				    (on-command=* (remove-component comp counter))))
+			(children comp)))
 	     (<:button (<:label= "Add")
-		       (on-command=*
-			 (add-component comp
-					(gensym)
-					(make-instance 'components-showcase))))
-	     (<:button (<:label= "Remove")
-		       (on-command=*
-			 (remove-component (parent comp) comp))))))
+		       (on-command=* (add-component comp (gensym)
+						    (make-instance 'counter :counter 0)))))))
+
+(define-component counter ()
+  ((counter :accessor counter
+	    :type integer
+	    :initform 0
+	    :initarg :counter))
+  (:render (counter)
+	   (<:label (<:value= (counter counter)))
+	   (<:button (<:label= "Increment")
+		     (on-command=* (increment-counter counter)))
+	   (<:button (<:label= "Decrement")
+		     (on-command=* (decrement-counter counter)))))
+
+(defun increment-counter (counter)
+  ;(break "Incrementing counter: ~A" counter)
+  (incf (counter counter)))
+
+(defun decrement-counter (counter)
+  ;(break "Decrementing counter: ~A" counter)
+  (decf (counter counter)))
