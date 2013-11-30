fe9f33e078febed8b6af7e7612ac09029111d12c
mmontone <marianomontone@gmail.com>
2013-11-30 14:10:30 -0300

    Housekeeping

--- src/controls/label.lisp
+++ src/controls/label.lisp
@@ -5,5 +5,5 @@
   (:documentation "This element is used to provide a label for a control element. If the user clicks the label, it will move the focus to the associated control, specified with the control attribute."))
 
 (define-xul-element image (xul-control)
-  ()
+  (onerror onload src validate)
   (:documentation "An element that displays an image, much like the HTML img element. The src attribute can be used to specify the URL of the image."))
