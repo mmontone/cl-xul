e3606f748ffd203a36b5161d456514dfe8d69513
mmontone <marianomontone@gmail.com>
2013-12-02 22:35:27 -0300

    Showcase: open component inside a dialog

--- test/showcase/dialogs.lisp
+++ test/showcase/dialogs.lisp
@@ -5,16 +5,37 @@
   (:render (comp)
 	   (<:vbox 
 	     (<:button (<:label= "Open dialog")
-		     (<:on-command=
-		       (xul::with-open-dialog
-			   ("Dialog test"
-			    '(:modal "yes"
-			      :resizable "no"))
-			 (<:dialog
-			  (<:id= "mydialog")
-			  (<:title= "My dialog")
-			  (<:buttons= "accept, cancel")
-			  (<:ondialogaccept= "alert('Accept!!');")
-			  (<:ondialogcancel= "alert('Cancel!!');")
-			  (<:box
-			    (<:description "This is a dialog")))))))))
+		       (<:on-command=
+			(xul::with-open-dialog
+			    ("Dialog test"
+			     '(:modal "yes"
+			       :resizable "no"))
+			  (<:dialog
+			    (<:id= "mydialog")
+			    (<:title= "My dialog")
+			    (<:buttons= "accept, cancel")
+			    (<:ondialogaccept= "alert('Accept!!');")
+			    (<:ondialogcancel= "alert('Cancel!!');")
+			    (<:box
+			      (<:description "This is a dialog"))))))
+	     (<:button (<:label= "Open component in dialog")
+		       (<:on-command=
+			(xul::with-open-dialog ("Component in dialog"
+						'(:modal "yes"
+						  :resizable "no"))
+			  (<:dialog
+			    (<:id= "mydialog")
+			    (<:title= "My dialog")
+			    (<:buttons= "accept, cancel")
+			    (<:ondialogaccept= "alert('Accept!!');")
+			    (<:ondialogcancel= "alert('Cancel!!');")
+			    (render (make-instance 'my-dialog))))))
+	     )))
+
+(define-component my-dialog ()
+  ()
+  (:render (comp)
+	   (<:vbox
+	     (<:description "This is my component in a dialog")
+	     (<:button (<:label= "Hello")
+		       (<:on-command= (ps:ps (alert "Hello!!")))))))
