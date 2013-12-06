2b8e0514d1a5fd0148eb5c737d62f4f327acbea9
mmontone <marianomontone@gmail.com>
2013-12-06 15:04:14 -0300

    Showcase: Lists multiple selection

--- test/showcase/lists.lisp
+++ test/showcase/lists.lisp
@@ -2,7 +2,9 @@
 
 (define-component lists-showcase ()
   ((single-state :initform "(no input yet)"
-		 :accessor single-state))
+		 :accessor single-state)
+   (multiple-state :initform nil
+		   :accessor multiple-state))
   (:render (comp)
 	   (<:vbox
 	     (render-list-states comp)
@@ -50,7 +52,33 @@
 	(single-state comp)))))
 
 (defun render-list-multiple-select (comp)
-  (declare (ignore comp)))
+  (<:group-box
+    (<:flex= 1)
+    (<:caption (<:label= "With multiple selection"))
+    (let ((items (list "Pearl" "Aramis" "Yakima" "Tribble" "Cosmo")))
+      (flet ((list-item (item)
+			(<:list-item (<:label= item)
+				     (when (member item
+						   (multiple-state comp))
+				       (<:selected= t)))))
+	(<:list-box
+	  (<:rows= 5)
+	  (<:seltype= :multiple)
+	  (on-select=* (index)
+	    (let ((item (nth index items)))
+	      (if (member item (multiple-state comp))
+		  (setf (multiple-state comp)
+			(remove item (multiple-state comp)))
+		  (push item (multiple-state comp)))))
+	  (loop for item in items
+	     do (list-item item)))))
+    (<:hbox
+      (<:pack= :center)
+      (<:description
+	(or (and
+	     (multiple-state comp)
+	     (princ-to-string (multiple-state comp)))
+	    "(no input yet)")))))
 
 (defun render-list-multiple-columns (comp)
   (declare (ignore comp)))
